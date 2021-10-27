07719d8257ed496981ca70df7a86bc38065bf6a4
Lucas Rocha <dev.lucascrocha@gmail.com>
2021-10-26 22:26:58 -0300

    finishes contact form

--- src/app/app-contact/app-contact.ts
+++ src/app/app-contact/app-contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '../base/base-component';
+import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
     selector: 'app-contact',
@@ -7,5 +8,21 @@ import { BaseComponent } from '../base/base-component';
     styleUrls: ['./app-contact.scss', '../../common.css']
 })
 export class AppContact extends BaseComponent {
-    
+    formSubmited: boolean = false;
+
+    contactForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+        super();
+
+        this.contactForm = this.formBuilder.group({
+            name: [''],
+            email: [''],
+            message: ['']
+        });
+    }
+
+    onSubmit() {
+        this.formSubmited = true;
+    }
 }
