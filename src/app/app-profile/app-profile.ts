708e5dfea1934d091e0c8938ced31286170f8a57
Lucas Rocha <dev.lucascrocha@gmail.com>
2021-10-29 20:07:28 -0300

    adds language changer

--- src/app/app-profile/app-profile.ts
+++ src/app/app-profile/app-profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '../base/base-component';
+import translationService from '../base/translation-service';
 import { isMobile, scrollTo } from '../base/util';
 
 @Component({
@@ -10,9 +11,17 @@ import { isMobile, scrollTo } from '../base/util';
 export class AppProfile extends BaseComponent {
     isMobile = isMobile();
 
+    currentLanguage = translationService.getCurrentSelectedLanguage();
+
     companyLink: string = 'https://www.nava.com.br/';
 
     linkClick(elementId: string) {
         scrollTo(elementId);
+    }
+
+    changeLanguage() {
+        translationService.setLanguage(this.currentLanguage == 'en' ? 'pt-BR' : 'en');
+
+        location.reload();
     }
 }
