b623d09b57d63a59aed730f291523ba82cca76f5
Lucas Rocha <dev.lucascrocha@gmail.com>
2022-02-22 20:26:20 -0300

    adds login pages and service

---
+++ app/src/app/app-login/app-login-service.ts
@@ -0,0 +1,6 @@
+class AppLoginService
+{
+
+}
+
+export default new AppLoginService();
---
+++ app/src/app/app-login/app-login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BaseComponent } from '../base/base-component';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './app-login.html',
+    styleUrls: ['./app-login.scss']
+})
+export class AppLogin extends BaseComponent {
+    
+}
