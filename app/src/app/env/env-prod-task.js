c06efb58612b632b8bf0ba3923d038a41c50ed5b
Lucas Rocha <dev.lucascrocha@gmail.com>
2022-02-25 19:54:47 -0300

    Adds docker file

--- app/src/app/env/env-prod-task.js
+++ app/src/app/env/env-prod-task.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-fs.copyFile('.\\src\\app\\env\\env-prod.ts', '.\\src\\app\\env\\env.ts', (err) => {
+fs.copyFile('./src/app/env/env-prod.ts', './src/app/env/env.ts', (err) => {
   if (err) throw err;
 });
