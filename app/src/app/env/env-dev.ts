3d6364637879fbd90a5bfcc6dda34f2cebcba456
Lucas Rocha <dev.lucascrocha@gmail.com>
2022-04-21 18:22:00 -0300

    removes angular code and adds stencil code

--- app/src/app/env/env-dev.ts
+++
@@ -1,8 +0,0 @@
-import { Env } from "./env-interface";
-
-const env: Env = {
-    apiurl: "https://localhost:57482",
-    imageUrl: "https://localhost:57482/v1/image?path="
-}
-
-export default env;
